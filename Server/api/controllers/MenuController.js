2894220df683b267198387d9931167298252e532
Zhihong <zl456@cornell.edu>
2015-04-08 14:53:25 -0400

    xx

---
+++ Server/api/controllers/MenuController.js
@@ -0,0 +1,11 @@
+/**
+ * MenuController
+ *
+ * @description :: Server-side logic for managing Menus
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
