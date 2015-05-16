b56cca9860c5c0a1bfdb0e97e9ce71c04a175cd1
xuke_fang git <fangxuke19@gamil.com>
2015-05-16 16:43:06 -0400

    added dish photos

--- Server/api/controllers/FileController.js
+++ Server/api/controllers/FileController.js
@@ -12,23 +12,20 @@ module.exports = {
             return res.json({
                 'status': 'GET not allowed'
             });
-        //	Call to /upload via GET is error
+        //  Call to /upload via GET is error
 
         var uploadFile = req.file('uploadFile');
-        UID = req.param('id');
         var data = req.params.all();
-        console.log(data);
-        var fn = req.file('uploadFile')._files[0].stream.filename;
-        var extension = fn.split('.').pop();
-
+        var filename = req.param('filename');
+        console.log("data" + data);
         uploadFile.upload({
             dirname: '../../assets/images',
-            saveAs: UID + "_profile." + extension
+            saveAs: filename
         }, function onUploadComplete(err, files) {
-            //	Files will be uploaded to .tmp/uploads
+            //  Files will be uploaded to .tmp/uploads
 
             if (err) return res.serverError(err);
-            //	IF ERROR Return and send 500 error with error
+            //  IF ERROR Return and send 500 error with error
             return res.json({
                 status: 200
             });
